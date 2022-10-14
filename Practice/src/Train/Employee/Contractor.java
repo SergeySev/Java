085b1e20d942831a3f6d088fcfd811d36192cb77
SergeySev <1sever6iv@gmail.com>
2022-10-14 13:21:33 +0200

    Contractor = 0

--- Practice/src/Train/Employee/Contractor.java
+++ Practice/src/Train/Employee/Contractor.java
@@ -2,8 +2,8 @@ package Train.Employee;
 
 public class Contractor extends Employee{
     public Contractor(String name,String surName, float salary) {
-        this.taxRate = 1;
-        this.socialRate = 1;
+        this.taxRate = 0;
+        this.socialRate = 0;
         this.salary = salary;
         this.name = name;
         this.surName = surName;
