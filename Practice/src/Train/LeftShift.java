b0106fb98e7fa3783f2d9d3372d07c8a19090400
SergeySev <1sever6iv@gmail.com>
2022-11-04 10:59:35 +0100

    refactor (LeftShift.java) : function leftShift

--- Practice/src/Train/LeftShift.java
+++ Practice/src/Train/LeftShift.java
@@ -5,23 +5,29 @@ import java.util.Arrays;
 public class LeftShift {
 
     public static int[] leftShift(int[] originArray, int shift) {
-        // сначала отнимает shift от длины до -1 // а потом заполняет с 0 до array - shift
+        /*
+        * Function shift array to number shift.
+        * leftShift({{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, 1) -> [11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        * leftShift({{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, 4) -> [8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7]
+         * leftShift({{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, array.length) -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+         * leftShift({{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, array.length - 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0]
+         * leftShift({{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, 0) -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+         * */
         int[] newArray = originArray.clone();
-        for (int i = 0; i < newArray.length - shift; i++) {
-            newArray[i + shift] = originArray[i];
-        }
-        for (int i = 0, j = shift; i < shift; i++, j--) {
-            newArray[i] = originArray[originArray.length - j];
+        for (int i = 0, j = shift; i < newArray.length; i++, j--) {
+            if (i < shift) {
+                newArray[i] = originArray[originArray.length - j];
+            } else newArray[i] = originArray[i - shift];
         }
         return newArray;
     }
 
     public static void main(String[] args) {
         int[] number = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int k = 3;
-        int[] number1 = {9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8};
-
-        System.out.println(Arrays.toString(leftShift(number, 3)));
-
+        System.out.println(Arrays.toString(leftShift(number, 1)));
+        System.out.println(Arrays.toString(leftShift(number, 4)));
+        System.out.println(Arrays.toString(leftShift(number, number.length)));
+        System.out.println(Arrays.toString(leftShift(number, number.length-1)));
+        System.out.println(Arrays.toString(leftShift(number, 0)));
     }
 }
