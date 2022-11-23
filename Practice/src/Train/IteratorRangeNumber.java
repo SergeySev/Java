e7cdd221c77feebee16b86ada6de277f0c770ef1
SergeySev <1sever6iv@gmail.com>
2022-11-23 10:46:49 +0100

    refactor (Worker.java) : add constructor Range for create Iterator

--- Practice/src/Train/IteratorRangeNumber.java
+++ Practice/src/Train/IteratorRangeNumber.java
@@ -8,15 +8,25 @@ public class IteratorRangeNumber {
                 Range.fromTo(2, 8)) {
             System.out.println(i);
         }
+
+        Range range = new Range(3, 8);
+        Iterator<Integer> it = range.iterator();
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
     }
 
 
     private static class Range implements Iterable<Integer> {
         int from;
         int to;
+
         public static Integer[] fromTo(int from, int to) {
-            Integer[] fromTo = new Integer[to-from];
-            for (int i = from, j = 0; i < to; i++, j++){
+            Integer[] fromTo = new Integer[to - from];
+            for (int i = from, j = 0; i < to; i++, j++) {
                 fromTo[j] = i;
             }
             return fromTo;
@@ -27,7 +37,12 @@ public class IteratorRangeNumber {
             return new Iterator<>(fromTo(from, to));
         }
 
+        public Range(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
     }
+
     static class Iterator<I extends Number> implements java.util.Iterator<Integer> {
         private final Integer[] array;
         private int point = 0;
